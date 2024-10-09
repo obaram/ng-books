52a2ab11b5aafb71384bece6c5f7a31e977f0de2
Mateusz Obara <mateuusz91@gmail.com>
2024-10-09 14:38:15 +0200

    feat: handle rest CRUD actions

--- ng-books/apps/ng-books/src/app/state/books.effects.ts
+++ ng-books/apps/ng-books/src/app/state/books.effects.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import {EMPTY, switchMap } from 'rxjs';
+import {EMPTY,  switchMap, tap} from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 import {BooksService} from "../services/books.service";
-import {getAllBooksSuccess, getBookSuccess} from "./actions";
+import {
+  createBook,
+  createBookSuccess, deleteBook, deleteBookSuccess, getAllBooks,
+  getAllBooksSuccess,
+  getBookSuccess,
+  navigateToCreateView,
+  navigateToEditView, updateBook, updateBookSuccess
+} from "./actions";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class BooksEffects {
   constructor(
     private actions$: Actions,
-    private booksService: BooksService
+    private booksService: BooksService,
+  private router: Router
   ) {}
 
   loadBooks$ = createEffect(() => this.actions$.pipe(
-      ofType('[Books] Get books'),
+      ofType(getAllBooks),
       switchMap(() => this.booksService.getBooks()
         .pipe(
           map(books => getAllBooksSuccess({payload: books})),
@@ -33,4 +42,55 @@ export class BooksEffects {
     )
     )
   );
+
+  navigateToEdit$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(navigateToEditView),
+        tap(({ bookId }) => {
+          this.router.navigate(['/edit', bookId]);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  navigateToCreate$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(navigateToCreateView),
+        tap(() => {
+          this.router.navigate(['/create']);
+        })
+      ),
+    { dispatch: false }
+  );
+
+  createBook$ = createEffect(() => this.actions$.pipe(
+    ofType(createBook),
+    switchMap((action) => this.booksService.addBook(action.book).pipe(
+      map(book => createBookSuccess({ book })),
+      catchError(() => EMPTY))
+    )));
+
+  updateBook$ = createEffect(() => this.actions$.pipe(
+    ofType(updateBook),
+    switchMap((action) => this.booksService.updateBook(action.book).pipe(
+      map(book => updateBookSuccess({ book })),
+      catchError((err) => {console.log(err); return EMPTY}))
+    )));
+
+  deleteBook$ = createEffect(() => this.actions$.pipe(
+    ofType(deleteBook),
+    switchMap((action) => this.booksService.deleteBook(action.bookId).pipe(
+      map(() => deleteBookSuccess({ bookId: action.bookId })),
+      catchError(()=> EMPTY))
+    )));
+
+  navigateToMain$ = createEffect(() => this.actions$.pipe(
+    ofType(updateBookSuccess, createBookSuccess),
+    tap(() => {
+      this.router.navigate(['/']); // Navigate to the main path
+    })
+  ), { dispatch: false });
+
 }
--- ng-books/apps/ng-books/src/app/views/books-edit/book-edit.component.ts
+++ ng-books/apps/ng-books/src/app/views/books-edit/book-edit.component.ts
@@ -1,13 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Book} from "../../types/book";
 import {BookFormComponent} from "../../components/book-form/book-form.component";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Store} from "@ngrx/store";
 import {AppState} from "../../types/app-state";
 import {Observable} from "rxjs";
 import {selectCurrentBook} from "../../state/books.selectors";
 import {AsyncPipe, CommonModule} from "@angular/common";
-import {getBook} from "../../state/actions";
+import {getBook, updateBook} from "../../state/actions";
 
 
 @Component({
@@ -20,7 +20,16 @@ export class BookEditComponent implements OnInit{
   book$!: Observable<Book | null>
   bookId!: number;
 
+  onSubmit(value: Book){
+    this.store.dispatch(updateBook({book: {...value, id: this.bookId}}))
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/']);
+  }
+
   constructor(private route: ActivatedRoute,
+              private router: Router,
               private store: Store<AppState>) {
   }
 
